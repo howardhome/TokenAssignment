61c892f7a5a9527a184a2a54d815cb8e75eeb325
Wenfeng (howard) Zhang <howard.zwf@gmail.com>
2018-11-19 16:48:11 -0500

    Add files via upload
    init files

---
+++ src/main/java/com/ctoboost/cassandra/whoop/WhoopApplication.java
@@ -0,0 +1,12 @@
+package com.ctoboost.cassandra.whoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ctoboost/cassandra/whoop/WhoopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ctoboost.cassandra.whoop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhoopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
